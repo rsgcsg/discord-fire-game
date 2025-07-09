ecf98756d56306d443960a42b72dbe147705a31a
sunguo chen <chens@MacBook-Air.local>
2025-07-09 16:40:16 +1000

    init

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+import discord
+from keep_alive import keep_alive
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Bot 已登录为 {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.lower().startswith('!ping'):
+        await message.channel.send('Pong!')
+
+keep_alive()  # 启动防止Render休眠的HTTP服务
+client.run(TOKEN)
